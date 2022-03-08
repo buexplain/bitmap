e0a2d945b1842892484101686184155ea398da7b
buexplain <buexplain@163.com>
2022-03-08 18:11:55 +0800

    解决并发实例化ClientFactory失败情况

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -57,10 +57,35 @@ class ClientFactory
 
     public static function getInstance(): ClientFactory
     {
-        if (static::$instance == null) {
-            static::$instance = new static();
+        //判断是否为协程环境
+        if (class_exists('\Swoole\Coroutine')) {
+            $cid = Coroutine::getCid();
+        } else {
+            $cid = 0;
         }
-        return static::$instance;
+        if (static::$instance === null) {
+            //拿到初始化的机会，先给个false，避免其它的协程也走到这一步，这一步是纯cpu操作，不会引起协程调度
+            static::$instance = false;
+            //初始化对象，并返回
+            static::$instance = new static();
+            return static::$instance;
+        }
+        //没有机会初始化的对象的协程，死循环等待其它协程初始化成功
+        $wait = 0;
+        while ($wait < 10) {
+            if ($cid > 0) {
+                //协程环境，进行休眠，让出cpu时间给初始化对象的协程使用
+                Coroutine::sleep(0.1);
+            }else{
+                usleep(100);
+            }
+            if (static::$instance instanceof ClientFactory) {
+                return static::$instance;
+            }
+            $wait++;
+        }
+        //等待次数太多，直接异常
+        throw new ReconnectException('Failed to instantiate ClientFactory object');
     }
 
     protected function createByProxy(): Client
